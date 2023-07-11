8125332bf709bafaadbffc20a59905b961daad26
Eric Son <eric.son04272006@gmail.com>
2023-07-10 21:56:13 -0400

    mongodb commit

--- src/main/java/com/csa/api/Application.java
+++ src/main/java/com/csa/api/Application.java
@@ -1,8 +1,11 @@
 package com.csa.api;
 
 import com.csa.api.domain.Student;
+import com.csa.api.repository.DatabaseService;
 import com.csa.api.ultil.JsonUtil;
+import com.mongodb.client.MongoCollection;
 import io.javalin.Javalin;
+import org.bson.Document;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,9 +29,53 @@ public class Application {
 
 
     public static void main(String[] args) {
-            var app = Javalin.create(/*config*/)
-                    .start(8080)
-                    .get("/", ctx -> ctx.result(JsonUtil.convertToJson(mockCreateStudents())));
+        DatabaseService databaseService = new DatabaseService();
+
+        var app = Javalin.create(/*config*/)
+                .start(8080)
+                .get("/", ctx -> ctx.result(JsonUtil.convertToJson(mockCreateStudents())));
+
+        app.get("/posts/question", ctx -> {
+            String response = "";
+            try {
+                MongoCollection<Document> posts = databaseService.getDb().getCollection("posts");
+                if (posts != null) {
+                    Document doc = posts.find().first();
+                    response = doc.toJson();
+                }
+                else {
+                    String errorMsg = "data not found";
+                    response = "{ \"error\" : \"" + errorMsg  + "\"}";
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+
+                String errorMsg = "Exception occurred";
+                response = "{ \"error\" : \"" + errorMsg  + "\"}";
+            }
+
+            ctx.json(response);
+        });
+
+        app.post("/posts/question", ctx -> {
+
+            try {
+                String bodyJson = ctx.body();
+                final Document document = Document.parse(bodyJson);
+                MongoCollection<Document> posts = databaseService.getDb().getCollection("posts");
+                if (posts == null) {
+                    databaseService.getDb().createCollection("posts");
+                }
+                else {
+                    databaseService.getDb().getCollection("posts").insertOne(document);
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
 
     }
 }
---
+++ src/main/java/com/csa/api/repository/DatabaseService.java
@@ -0,0 +1,49 @@
+package com.csa.api.repository;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class DatabaseService {
+
+    private MongoClient mongoClient;
+
+    private MongoDatabase db;
+
+    public DatabaseService() {
+        initializeDB();
+    }
+
+    public MongoDatabase getDb() {
+        return db;
+    }
+
+    private void initializeDB() {
+        String user = "csa-api-user";
+        String password = "test123";
+        String databaseName = "csa-api-db";
+        String connectionStringUri = "mongodb+srv://" + user + ":" + password + "@cluster0.fpb9ni4.mongodb.net/?retryWrites=true&w=majority";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionStringUri))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try {
+            mongoClient = MongoClients.create(settings);
+            // Send a ping to confirm a successful connection
+            db = mongoClient.getDatabase(databaseName);
+            db.runCommand(new Document("ping", 1));
+
+            String status = "Pinged your deployment. You successfully connected to MongoDB!";
+            System.out.println("status = " + status);
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+    }
+}
